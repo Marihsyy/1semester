3953f30657fe407574df0a683f2e15c2ef3cc5ba
Марійка Пахолок <marichkaCo@gmail.com>
2026-09-10 18:44:42 +0300

    lab1

---
+++ pr1_pakholok.py
@@ -0,0 +1,24 @@
+
+print("Hello, World!")
+
+a = 16 
+b = 1.58 
+c = "Привіт" 
+d = True
+
+print(a,type(a))
+print(b,type(b))
+print(c,type(c))
+print(d,type(d))
+
+a = 10
+b = 5
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b) 
+print(a // b) 
+print(a % b) 
+print(a ** b)  
+
