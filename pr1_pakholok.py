1770c3944b40fc41dafe1fde9f987c3225d92096
Марійка Пахолок <marichkaCo@gmail.com>
2026-09-17 22:15:53 +0300

    Enhance pr1_pakholok.py with additional variables and prints
    Added new variables and print statements to demonstrate data types and arithmetic operations.

--- pr1_pakholok.py
+++ pr1_pakholok.py
@@ -1,24 +1,36 @@
-
 print("Hello, World!")
 
-a = 16 
-b = 1.58 
-c = "Привіт" 
+
+a = 16
+b = 1.58
+c = "Привіт"
 d = True
+e = ["кіт","собака","папугай"]
+f = (50.70, 25.34)
+g = {"синій","рожевий","фіолетовий"}
+h = {
+    "name": "Mariia",
+    "age": 16,
+    "student": True
+}
 
 print(a,type(a))
 print(b,type(b))
 print(c,type(c))
 print(d,type(d))
+print(e,type(e))
+print(f,type(f))
+print(g,type(g))
+print(h,type(h))
 
-a = 10
-b = 5
 
-print(a + b)
-print(a - b)
-print(a * b)
-print(a / b) 
-print(a // b) 
-print(a % b) 
-print(a ** b)  
+x = 10
+y = 3
 
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x//y)
+print(x%y)
+print(x**y)
